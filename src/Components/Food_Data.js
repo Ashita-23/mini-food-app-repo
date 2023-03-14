44b3d39b171d751d33bcfc567f056a289e211ede
Akash <akashbytes@gmail.com>
2023-03-14 23:43:41 +0530

    first commit uploding mini food app to git hub

---
+++ src/Components/Food_Data.js
@@ -0,0 +1,151 @@
+const foodData = [
+    {
+      id: 1,
+      altname: "pan Cake",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "Pan cake ",
+      discription: "breakfast",
+      imgs:
+        "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 2,
+      altname: "fruit saled",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "fruit saled ",
+      discription: "breakfast",
+      imgs:
+        "https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 3,
+      altname: "vegetable saled",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "vegetable saled ",
+      discription: "breakfast",
+      imgs:
+        "https://images.pexels.com/photos/551997/pexels-photo-551997.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 4,
+      altname: "garlic bread",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "garlic bread ",
+      discription: "breakfast",
+      imgs:
+        "https://images.pexels.com/photos/7664397/pexels-photo-7664397.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 5,
+      altname: "bolied eggs",
+      vegOrnonveg: "Non-Veg ",
+      foodnames: "boiled eggs ",
+      discription: "breakfast",
+      imgs:
+        "https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 6,
+      altname: "pasta",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "pasta",
+      discription: "lunch",
+      imgs:
+        "https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 7,
+      altname: "Shushee",
+      vegOrnonveg: "Non-Veg ",
+      foodnames: "Shushee",
+      discription: "Lunch",
+      imgs:
+        "https://images.pexels.com/photos/3642030/pexels-photo-3642030.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 8,
+      altname: "Pitzaa",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "Pitzaa ",
+      discription: "Lunch",
+      imgs:
+        "https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+
+    {
+      id: 9,
+      altname: "Burgger",
+      vegOrnonveg: "pure Veg ",
+      foodnames: "Burgger ",
+      discription: "Lunch",
+      imgs:
+        "https://images.pexels.com/photos/2657960/pexels-photo-2657960.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 10,
+      altname: "egg curry",
+      vegOrnonveg: "Non-Veg ",
+      foodnames: "egg curry",
+      discription: "Lunch",
+      imgs:
+        "https://images.pexels.com/photos/4940719/pexels-photo-4940719.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 11,
+      altname: "chiken",
+      vegOrnonveg: "Non-Veg ",
+      foodnames: "chiken",
+      discription: "Dinner",
+      imgs:
+        "https://images.pexels.com/photos/6210747/pexels-photo-6210747.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 12,
+      altname: "crad",
+      vegOrnonveg: "Non-Veg ",
+      foodnames: "Crab",
+      discription: "Dinner",
+      imgs:
+        "https://images.pexels.com/photos/1998920/pexels-photo-1998920.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 13,
+      altname: "veg-pasta",
+      vegOrnonveg: "Veg ",
+      foodnames: "veg-pasta",
+      discription: "Dinner",
+      imgs:
+        "https://images.pexels.com/photos/3297363/pexels-photo-3297363.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  
+    {
+      id: 14,
+      altname: "chesszy pitzaa",
+      vegOrnonveg: "Veg ",
+      foodnames: "chesszy pitzaa",
+      discription: "Dinner",
+      imgs:
+        "https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    
+    {
+      id: 15,
+      altname: "prons",
+      vegOrnonveg: "Non-Veg ",
+      foodnames: "prons",
+      discription: "Dinner",
+      imgs:
+        "https://images.pexels.com/photos/1031780/pexels-photo-1031780.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    }
+  ]
+  
---
+++ src/Components/Menu-Inpu.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const MenuCard = ({ menuData }) => {
+
+
+   
+  return (
+ 
+       <>
+       { menuData.map((value)=>{
+        return(
+            <>
+            
+            <div className="card-outer" key={value.id}>
+              <div className="card-head">
+                <span className="serial-no-box">{value.id}</span>
+                <span className="food-time-box">{value.discription}</span>
+              </div>
+              <div className="card-main">
+                <img
+                  src={value.imgs}
+                  alt={value.altname}
+                  className="img-box"
+                />
+                <h2 className="food-name-box">{value.foodnames}</h2>
+                <p className="food-vegOrnon-box">{value.vegOrnonveg}</p>
+                <p className="discription-box">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui culpa
+                  odit vel aliquid repellendus voluptate illo harum eaque! Minima
+                  repudiandae sint vero natus eos fuga!
+                </p>
+                <button className="Order-btn">Order</button>
+              </div>
+            </div> 
+          </>
+              
+        )
+       })}
+       </>
+ 
+ 
+  )
+};
+
+export default MenuCard;
